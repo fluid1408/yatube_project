5cdccec1ccac24f506c8eb4b894a5aa59b638c16
Сергей Шлемин <schlyomin@yandex.ru>
2022-08-20 21:14:35 +0300

    Добавление функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+def index(request):
+    template = "index.html"
+    return render(request, template)
 
 # Главная страница
 def index(request):    
